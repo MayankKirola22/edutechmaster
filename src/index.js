563aa5eed0c9e6460e0a684ec95e68343daccdde
Mayank Kirola <mayankkirola22@gmail.com>
2024-05-06 01:37:45 +0530

    updated clientID

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId='231571534208-106t1i810n0e77tp5j2mg5qn5mguk2hk.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId='231571534208-bri8khfr7s3746qfso7lj92ng7bt0em5.apps.googleusercontent.com'>
     <BrowserRouter>
       <App />
     </BrowserRouter>
